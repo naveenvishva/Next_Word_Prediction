297c4c268aff0f725f8e960f49ee3e9ff6e9d6a1
NAVEENRAJ B <80468233+naveenvishva@users.noreply.github.com>
2024-05-02 19:18:09 +0530

    Update nextword.py

--- nextword.py
+++ nextword.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -6,41 +5,49 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
 
-# Load text data from environment variable or URL
-text_path = os.getenv('sherlock-holm.es_stories_plain-text_advs.txt')
-text = requests.get(text_path).text
-
-# Tokenize the text
+# Read the text file
+with open(r'sherlock-holm.es_stories_plain-text_advs.txt', 'r') as file:
+    text = file.read()
+    
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts([text])
 total_words = len(tokenizer.word_index) + 1
 
-# Generate input sequences
 input_sequences = []
 for line in text.split('\n'):
     token_list = tokenizer.texts_to_sequences([line])[0]
     for i in range(1, len(token_list)):
         n_gram_sequence = token_list[:i+1]
         input_sequences.append(n_gram_sequence)
-
-# Pad sequences
+        
 max_sequence_len = max([len(seq) for seq in input_sequences])
 input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
 X = input_sequences[:, :-1]
 y = input_sequences[:, -1]
 
-# One-hot encode labels
 y = np.array(tf.keras.utils.to_categorical(y, num_classes=total_words))
 
-# Define and compile the model
 model = Sequential()
 model.add(Embedding(total_words, 100, input_length=max_sequence_len-1))
 model.add(LSTM(150))
 model.add(Dense(total_words, activation='softmax'))
+print(model.summary())
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=1, verbose=1)
 
-# Train the model
-model.fit(X, y, epochs=100, verbose=1)
+seed_text = "I will leave if they"
+next_words = 3
 
-# Save the model (optional)
-model.save('text_generation_model.h5')
+for _ in range(next_words):
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted = np.argmax(model.predict(token_list), axis=-1)
+    output_word = ""
+    for word, index in tokenizer.word_index.items():
+        if index == predicted:
+            output_word = word
+            break
+    seed_text += " " + output_word
+
+print(seed_text)
